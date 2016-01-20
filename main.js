897a2ae8d76e5f866a35fc99e57f493c30089e31
Student <student@RU-004.local>
2016-01-20 16:31:23 -0700

    lightbulb added with hover

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('main-controller',['$scope', function($scope){
-
+myApp.controller('main-controller',['$scope','$timeout', function($scope, $timeout){
+	$scope.match = false;
 	$scope.greeting = 'RELIEF EFFORT'
-
 	$scope.victims = []
 	$scope.volunteers = []
 
 	$scope.addVictim = function(){
+		
 		$scope.victims.push($scope.newVictim);
 		$scope.newVictim = {};
 		console.log($scope.victims)
+		$scope.checkMatch();
+
 	}
 
 	$scope.addVol = function(){
+		
 		$scope.volunteers.push($scope.newVol);
 		$scope.newVol = {}
 		console.log($scope.volunteers)
+		
 	}
 
-	
+	$scope.checkMatch = function(){
+		for (i = 0; i < $scope.volunteers.length; i++) {
+		if ($scope.victims[$scope.victims.length - 1].address === $scope.volunteers[i].address){
+			$scope.volunteers[i].match = true;
+		}
+
+	}
+	}
 
 
 
-
-
+// scope.volunteers[0].address
 
 
 }])
